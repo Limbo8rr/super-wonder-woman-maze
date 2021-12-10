c2d182e71da6fc308c07a125b19cf30582857f23
Limbo8rr <77548425+Limbo8rr@users.noreply.github.com>
2021-12-09 19:38:05 -0800

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,21 @@
-scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
+namespace SpriteKind {
+    export const wondy = SpriteKind.create()
+}
+scene.onOverlapTile(SpriteKind.wondy, assets.tile`myTile1`, function (sprite, location) {
+    info.changeScoreBy(1)
+    tiles.setTileAt(location, assets.tile`transparency16`)
+})
+scene.onOverlapTile(SpriteKind.wondy, assets.tile`chorona 0`, function (sprite, location) {
+    info.changeScoreBy(-10)
+    music.buzzer.play()
+    tiles.setTileAt(location, assets.tile`myTile1`)
+})
+scene.onOverlapTile(SpriteKind.wondy, assets.tile`myTile2`, function (sprite, location) {
     info.changeScoreBy(1)
     music.bigCrash.play()
     tiles.setTileAt(location, assets.tile`transparency16`)
 })
-scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
-    info.changeScoreBy(1)
-    tiles.setTileAt(location, assets.tile`transparency16`)
-})
-scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairLarge, function (sprite, location) {
+scene.onOverlapTile(SpriteKind.wondy, sprites.dungeon.stairLarge, function (sprite, location) {
     game.over(true)
 })
 let mySprite = sprites.create(img`
@@ -27,13 +35,13 @@ let mySprite = sprites.create(img`
     . . . . . . 5 8 8 8 5 . . . . . 
     . . . . . . . 2 2 2 . . . . . . 
     . . . . . . . 2 . 2 . . . . . . 
-    `, SpriteKind.Player)
+    `, SpriteKind.wondy)
 controller.moveSprite(mySprite, 90, 90)
 scene.setBackgroundColor(11)
 tiles.setTilemap(tilemap`level1`)
 tiles.placeOnRandomTile(mySprite, sprites.dungeon.stairNorth)
 scene.cameraFollowSprite(mySprite)
 forever(function () {
-    music.playMelody("" + "E A G G A G A B " + "D F A F D A G G " + "A F A E G D F E " + "" + "A F A E - D F E ", 70)
+    music.playMelody("" + "E A G G A G A B " + "D F A F D A G G " + "A F A E G D F E " + "" + "", 70)
     music.playMelody("A F A E - D F E ", 200)
 })
